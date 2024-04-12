a2d7f4397d51fd32c94bff751c6b87cd89dc017d
Amer Alhabsi <amer.alhabsi@gmail.com>
2024-04-12 06:32:18 +0400

    added comments and validation test vectors.

--- timestamp.h
+++ timestamp.h
@@ -17,5 +17,4 @@ uint32_t jd(int y, int m, int d);
 uint32_t unix_time_stamp(int y, int m, int d, int h, int mi, int s);
 uint64_t adtime(struct dtm z);
 int adtime_inv(uint64_t z, struct dtm *m);
-
 #endif
